97b1f6ed8a9441eabfb9feaca3c11a79bb00487e
Daniel <deraingo3@gmail.com>
2026-05-29 23:01:08 -0600

    fucked up the tag

--- client/src/components/FinalsRouletteSection.jsx
+++ client/src/components/FinalsRouletteSection.jsx
@@ -3,7 +3,7 @@ function FinalsRouletteSection(){
     <>
     <div className="app-section">
         <h2 className="app-title">Finals Roulette</h2>
-        <p>To add you your twitch press the <i>add bot to your twitch</i>> button</p>
+        <p>To add you your twitch press the <i>add bot to your twitch</i>button</p>
         <p>Then add a new channel point redeem called "Random Loadout"</p>
         <i>I will be changing this to hopefully ADD the redeem too but that is a WiP</i>
         <div id="finals-roulette-section" className="grid sm:grid-cols-2 lg:grid-cols-2 gap-5">
